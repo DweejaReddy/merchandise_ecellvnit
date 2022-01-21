fcb43edcdf08059512fb65f02b85bab054ef1f7b
DweejaReddy <74244501+DweejaReddy@users.noreply.github.com>
2022-01-21 05:54:32 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link
+        rel="stylesheet"
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+        crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="merchstyle.css">
+</head>
+<body>
+    <nav id="mainNavbar" class="navbar navbar-light navbar-expand-lg py-0 sticky-top"data-aos="fade-down"
+    data-aos-duration="2000">
+      <a href="https://www.ecellvnit.org/" class="navbar-brand">
+      <img class="d-none d-lg-block ml-5" src="img/E-Cell_logo.png" alt="ecell" width="160px">
+      <img class="d-sm d-lg-none ml-3" src="img/E-Cell_logo.png" alt="ecell" width="120px"></a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navLinks"
+        aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navLinks">
+        <ul class="navbar-nav text-center">
+          <li class="nav-item d-none d-lg-block">
+            <a href="index.php" class="nav-link">Home</a>
+          </li>
+          <li class="nav-item d-lg-none">
+            <a href="index.php" class="nav-link">Home</a>
+          </li>
+          <li class="nav-item">
+            <a href="index.php" class="nav-link">Events</a>
+          </li>
+          <li class="nav-item">
+           <a href="index.php" class="nav-link">Team</a>
+          </li>
+          <li class="nav-item">
+            <a href="index.php" class="nav-link" target="_blank">Contact Us</a>
+          </li>
+        </ul>
+        <li class="nav-item d-none d-lg-block" style="margin-right: 10%;">
+          <a class="btn btn-dark btn1 mx-4 px-4" href="" target="_blank" role="button">Buy Merchandise</a>
+        </li>
+      </div>
+    </nav>
+
+
+    <script
+  src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+  integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+  crossorigin="anonymous"
+  ></script>
+  <script
+  src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+  integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+  crossorigin="anonymous"
+  ></script>
+  <script
+  src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+  integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+  crossorigin="anonymous"
+  ></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Merchandise | E-CELL, VNIT</title>
+</head>
+<?php include('header.php')?>
+<body style="background-color: #232323;">
+    <div class="bord"></div>
+    <div class="name">E-CELL VNIT</div>
+    <div class="bord1"></div>
+    <div class="bord2"></div>
+    <section id="home" class="d-none d-md-block">
+    <img src="img/home.png" style="width: 100vw;">
+    <img class=" home_shirt" src="img/home_tshirt.png">
+    <a class="btn btn2" href="" target="_blank" role="button">BUY NOW</a>
+</section>
+
+<section id="home_mob" class="d-block d-md-none container text-center align-items-center">
+    <img class=" home_mob_shirt" src="img/home_tshirt.png">
+    <a class="btn btn2_mob text-center" href="" target="_blank" role="button">BUY NOW</a>
+</section>
+
+<section class="container bg my-5 py-5 text-center align-items-center mx-auto" style="max-width: 85vw;">
+    <div class="row align-items-center mx-auto">
+        <div class="col-lg-6 col-12 my-4">
+            <img class="front " src="img/Tshirt_front.png" alt="">
+            <h4 class="text-center text-white sub my-4">FRONT</h4>
+        </div>
+        <div class="col-lg-6 col-12 my-4">
+            <img class="back " src="img/Tshirt_back.png" alt="">
+            <h4 class="text-center text-white sub my-4">BACK</h4>
+        </div>
+       
+        <div class="col-md-12 text-center my-2">
+            <a href="" target="_blank"><button type="button" class="btn text-white btn3">BUY NOW</button></a>
+        </div>
+    </div>
+</section>
+
+<section class="container my-5 text-center align-items-center mx-auto">
+    <h4 class="heading d-block d-lg-none">OFFICIAL E-CELL MERCHANDISE</h4>
+    <div class="row align-items-center">
+        <div class="col-12 col-lg-6 my-4">
+            <img class="model" src="img/model.png" alt="">
+        </div>
+        <div class="col-12 col-lg-5 my-auto">
+            <h2 class="heading d-none d-lg-block my-2 text-left">OFFICIAL E-CELL MERCHANDISE</h2>
+            <h4 class="text-white text-xl-left" style="font-weight: 400;">Half Sleeve T-Shirt</h4>
+            <h4 class="text-white text-xl-left" style="font-weight: 700;"><span class="mr-3"><i class="fas fa-rupee-sign"></i> 349 </span><span style="color: #919191;" class="strikethrough"><i class="fas fa-rupee-sign"></i> 799</span><span style="font-weight: 400;">(55% OFF)</span></h4>
+            <h4 class="mt-5 mb-4 text-xl-left" style="color: #919191;font-weight: 400;">Sizes Available</h4>
+            <div class="text-xl-left sizebtn" style="color: #919191;display: inline;">
+                <span class="sizes text-lg-left" style="padding: 10px 17px;">S</span>
+                <span class="sizes text-lg-left" style="padding: 10px 14px;">M</span>
+                <span class="sizes text-lg-left" style="padding: 10px 17px;">L</span>
+                <span class="sizes text-lg-left" style="padding: 10px 13px;">XL</span>
+                <span class="sizes text-lg-left" style="padding: 10px 10px;">XXL</span>
+            </div>
+            <div class="col-md-12 text-xl-left my-5">
+                <a href="" target="_blank"><button type="button" class="btn text-white btn3">BUY NOW</button></a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="container bg my-5 py-5 text-center align-items-center mx-auto">
+    <div class="row align-items-center mx-auto">
+        <div class="col-lg-6 col-12 my-4">
+            <img class="mask" src="img/mask.png" alt="">
+        </div>
+        <div class="col-lg-6 col-12 my-4">
+            <div class="group"></div>
+            <div class="text-xl-left free">FREE</div>
+            <div class="text-xl-left para">2 MASKS WITH EACH PURCHASE</div>
+        </div>
+    </div>
+</section>
+</body>
+</html>
