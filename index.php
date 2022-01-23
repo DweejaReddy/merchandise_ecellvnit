677fdba7f91c4aa88ebcce8a4965af44e20b3c92
DweejaReddy <74244501+DweejaReddy@users.noreply.github.com>
2022-01-23 22:26:28 +0530

    Update index.php

--- index.php
+++ index.php
@@ -52,7 +52,7 @@
             <h3 class="heading d-none d-lg-block my-2 text-left ml-xl-3">OFFICIAL E-CELL MERCHANDISE</h3>
             <h4 class="text-white text-xl-left ml-xl-3" style="font-weight: 400;">Half Sleeve T-Shirt</h4>
             <h4 class="text-white text-xl-left ml-xl-3" style="font-weight: 700;"><span class="mr-3"><i class="fas fa-rupee-sign"></i> 299 </span><span style="color: #919191;" class="strikethrough"><i class="fas fa-rupee-sign"></i> 499</span><span style="font-weight: 400;">(40% OFF)</span><span style="color: #919191;font-weight: 400;font-size: 1.3rem;"> Without Name</span></h4>
-            <h4 class="text-white text-xl-left ml-xl-3" style="font-weight: 700;"><span class="mr-3"><i class="fas fa-rupee-sign"></i> 349 </span><span style="color: #919191;" class="strikethrough"><i class="fas fa-rupee-sign"></i> 499</span><span style="font-weight: 400;">(30% OFF)</span><span style="color: #919191;font-weight: 400;font-size: 1.3rem;"> With Name</span></h4>
+            <h4 class="text-white text-xl-left ml-xl-3" style="font-weight: 700;"><span class="mr-3"><i class="fas fa-rupee-sign"></i> 349 </span><span style="color: #919191;" class="strikethrough"><i class="fas fa-rupee-sign"></i> 549</span><span style="font-weight: 400;">(40% OFF)</span><span style="color: #919191;font-weight: 400;font-size: 1.3rem;"> With Name</span></h4>
             <h4 class="mt-5 mb-4 text-xl-left ml-xl-3" style="color: #919191;font-weight: 400;">Sizes Available</h4>
             <div class="text-center d-xl-flex flex-row d-none sizebtn ml-xl-3" style="color: #919191;">
                 <span class="sizes text-lg-left mr-1" style="padding: 10px 19px;">S</span>
@@ -61,32 +61,32 @@
                 <span class="sizes text-lg-left mr-1" style="padding: 10px 15px;">XL</span>
                 <span class="sizes text-lg-left mr-1" style="padding: 10px 10px;">2XL</span>
                 <span class="sizes text-lg-left mr-1" style="padding: 10px 10px;">3XL</span>
-                <span class="sizes text-lg-left mr-1" style="padding: 10px 10px;">4XL</span>
+                <!-- <span class="sizes text-lg-left mr-1" style="padding: 10px 10px;">4XL</span> -->
             </div>
             <div class="row align-items-center d-flex justify-content-center d-lg-none" style="color: #919191;">
                 <div class="col-3">
-                    <span class="sizes" style="padding: 10px 19px;">S</span>
+                    <span class="sizes mx-auto" style="padding: 10px 19px;">S</span>
                 </div>
                 <div class="col-3">
-                    <span class="sizes" style="padding: 10px 19px;">M</span>
+                    <span class="sizes mx-auto" style="padding: 10px 19px;">M</span>
                 </div>
                 <div class="col-3">
-                    <span class="sizes " style="padding: 10px 19px;">L</span>
+                    <span class="sizes mx-auto" style="padding: 10px 19px;">L</span>
                 </div>
                 <div class="col-3">
-                    <span class="sizes" style="padding: 10px 13px;">XL</span>
+                    <span class="sizes mx-auto" style="padding: 10px 13px;">XL</span>
                 </div>
             </div>
             <div class="row align-items-center d-flex justify-content-center d-lg-none" style="color: #919191;">
-                <div class="col-3  mt-4">
-                    <span class="sizes" style="padding: 10px 13px;">2XL</span>
+                <div class="col-3  mt-4 mx-auto">
+                    <span class="sizes" style="padding: 10px 11px;">2XL</span>
                 </div>
-                <div class="col-3 mt-4">
-                    <span class="sizes" style="padding: 10px 13px;">3XL</span>
+                <div class="col-3 mt-4 mx-auto">
+                    <span class="sizes" style="padding: 10px 11px;">3XL</span>
                 </div>
-                <div class="col-3 mt-4">
+                <!-- <div class="col-3 mt-4 mx-auto">
                     <span class="sizes" style="padding: 10px 13px;">4XL</span>
-                </div>
+                </div> -->
             </div>
             <div class="col-md-12 text-xl-left my-5 ml-xl-3">
                 <a href="register.php" target="_blank"><button type="button" class="btn text-white btn3">BUY NOW</button></a>
@@ -116,7 +116,7 @@
             <h4 class="text-center text-white sub my-4">FRONT</h4>
         </div>
         <div class="col-lg-6 col-12 my-4">
-            <img class="back px-xl-5 py-4" src="img/Tshirt-back complete 1.png" alt="">
+            <img class="back px-xl-5 py-4" src="img/Tshirt-back complete(alumni) 1.png" alt="">
             <h4 class="text-center text-white sub my-4">BACK</h4>
         </div>
        
@@ -126,7 +126,7 @@
     </div>
 </section>
 
-<section class="container align-items-center">
+<section class="container align-items-center my-5">
     <h1 class="heading my-2 text-center">Size Chart</h1>
     <img style="margin: 0 auto;" width="100%" src="img/chart.png" alt="">
 </section>
